3e22fe957902a8ef8be1255bbc15109c9c1c05db
Nick Hughes <nick@nickhughes.io>
2018-11-21 16:12:40 +1100

    Input mechanism

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # Main file
+import logging
+
+print("Welcome to the land of Bojangles, what do you wish to do? \n")
+
+u_input = input()
+u_verb = u_input.split()[0]
+u_action = u_input.split()[1:len(u_input.split())]
+
+
+
+logging.info("Input Verb = " + u_verb)
+logging.info("Input Actions = ")
+logging.info(u_action)
+
+
+# print("Input Verb = " + u_verb)
+# print("Input Actions = ")
+# print(u_action)
