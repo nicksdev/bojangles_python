946d8499d8bfb73c44090740fde4c753b0be17e7
Nick Hughes <nick@nickhughes.io>
2018-11-28 20:16:17 +1100

    Loading rooms and initial state setup

--- main.py
+++ main.py
@@ -2,33 +2,61 @@
 import json
 import logging
 
-
-
 logging.basicConfig(filename='info.log', filemode='w', level=logging.INFO)
 
 
-json_data = open('map.json')
-x = json.load(json_data)
-json_data.close()
-print(x)
-print(type(x))
+temp_data = open('map.json')
+mapdata = json.load(temp_data)
+temp_data.close()
 
-
+# room = mapdata['room1']
+# print(room)
+# print(room['name'])
+# print(room.keys())
+# print(room.values())
+#
+# print(type(mapdata))
 
 
 print("Welcome to the land of Bojangles, what do you wish to do? \n")
+
+
+#FUNCTION LIBRARY
+
+def loadroom(room):
+    roomdata = mapdata[room]
+    #print('Loading Room: ' + roomdata['name'])
+    logging.info('Loading Room: ' + roomdata['name'])
+    print(roomdata['description'])
+    print('You can see the following exits: ')
+    print(' '.join(roomdata['exit']))
+    print('What would you like to do now?')
+
+#Initialise room1
+
+
+location = 'room1'
+
+loadroom(location)
+
 
 u_input = input()
 u_verb = u_input.split()[0]
 u_action = u_input.split()[1:len(u_input.split())]
 
-
-
 logging.info("Input Verb = " + u_verb)
-logging.info("Input Actions = ")
-logging.info(u_action)
+logging.info("Actions = %s" % ' ' .join(map(str, u_action)))
+
+if u_verb == "go":
+    print("You go somewhere")
+    roomdata = mapdata[location]
+    #print(location)
+    #print(u_action[0])
+    print(roomdata['exit2'][u_action[0]])
+    loadroom(roomdata['exit2'][u_action[0]])
+
+else:
+    print("I don't recognise that action")
 
 
-# print("Input Verb = " + u_verb)
-# print("Input Actions = ")
-# print(u_action)
+
--- notes.md
+++ notes.md
@@ -1,2 +1,12 @@
 TO display logs in the terminal:
 tail -f info.log
+
+
+
+Initialise Room:
+Pass in room id
+Call room description
+
+
+COMBAT:
+Consider while loop with break, continue conditions
